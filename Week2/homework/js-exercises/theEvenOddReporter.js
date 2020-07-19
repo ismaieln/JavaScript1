05cce53d9ecace3cffd25c44adabde156279183d
ismaiel <ismaiel.nab@gmail.com>
2020-07-19 20:39:59 +0200

    complete the file theEvenOddReporter.js

---
+++ Week2/homework/js-exercises/theEvenOddReporter.js
@@ -0,0 +1,12 @@
+// (1) create for loop
+
+for (let i = 0; i < 20; i++) {
+  // (2) create condition to check the number
+  if (i % 2 == 0) {
+    // (3) if it is even
+    console.log("The number" + i + "is even!");
+  } else {
+    // (4) if it is odd
+    console.log("The number" + i + "is odd!");
+  }
+}
