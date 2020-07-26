25876bf7695a0a616697421161a7631ef77eadf4
ismaiel <ismaiel.nab@gmail.com>
2020-07-26 14:19:46 +0200

    create and complete youAreAmazing.js first execise

---
+++ Week3/homework/js-exercises/youAreAmazing.js
@@ -0,0 +1,31 @@
+for (let i = 1; i <= 3; i++) {
+  let myName = "Ismaiel";
+  giveCompliment(myName);
+}
+
+function giveCompliment(aName) {
+  let compliments = [
+    "great",
+    "awesome",
+    "smart",
+    "nice",
+    "amazing",
+    "so smart",
+    "wonderful",
+    "lovely",
+    "pretty",
+    "the best",
+  ];
+  let i;
+  i = getTheNumber(i);
+  console.log("You are " + compliments[i] + ", " + aName + "!");
+  return true;
+}
+
+// this is a function to help get a random number(0 to 9)
+function getTheNumber(som) {
+  let ran = Math.random();
+  som = Math.round(ran * 10);
+  // som is a number between 0 and 9
+  return som;
+}
