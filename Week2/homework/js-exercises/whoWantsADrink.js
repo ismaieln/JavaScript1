1b8088aa21e123e1235f43df1f7645cda1f17d90
ismaiel <ismaiel.nab@gmail.com>
2020-07-27 20:22:17 +0200

    update the whoWantsADrink.js as per the comments from the mentor

--- Week2/homework/js-exercises/whoWantsADrink.js
+++ Week2/homework/js-exercises/whoWantsADrink.js
@@ -1,34 +1,28 @@
 // (1)
+// declare variables
 let drinkTray = [];
-
-const drinkTypes = ["cola", "lemonade", "water"];
+let keys = [];
+let drinkTypes = ["cola", "lemonade", "water"];
 
 // (2) (3) (4)
-
-let counter = 0;
+let max = 2; // to control max number of eatch drink on tray
+let temp;
 
 for (let i = 0; i < 5; i++) {
-  //condition to keep the counter les than the array length
-  if (counter <= drinkTypes.length) {
-    // condition to store the first item
-    if (i == 0) {
-      drinkTray.push(drinkTypes[counter]);
-    } else {
-      // only to hold at most two instances of the same drink type
-      for (let ii = 0; ii <= i; ii++) {
-        if (drinkTray[ii] == drinkTray[i]) {
-          counter++;
-        } else {
-          drinkTray.push(drinkTypes[counter]);
-        }
-      }
-    }
-  } else {
-    counter = 0;
-  }
-}
+  keys.push(getR());
+  temp = keys[i];
 
-// (5)
+  // keep doing until ok
+  while (doAgain()) {
+    doAgain();
+  }
+  keys[i] = temp;
+}
+// to load the drinks on tray
+for (let y = 0; y < 5; y++) {
+  drinkTray[y] = drinkTypes[keys[y]];
+}
+// to log the required message
 let counts = "Hey guys, I brought a ";
 
 for (let index = 0; index < 5; index++) {
@@ -38,4 +32,24 @@ for (let index = 0; index < 5; index++) {
     counts += drinkTray[index] + ", ";
   }
 }
+
 console.log(counts);
+
+// to bick a random number (0-2)
+function getR() {
+  return Math.floor(Math.random() * 3);
+}
+// to check the repeated items
+function ifEqual(value) {
+  return value == temp;
+}
+// to change if repeated more than the max
+function doAgain() {
+  let equalA = keys.filter(ifEqual);
+  if (equalA.length > max) {
+    temp = getR();
+    return true;
+  } else {
+    return false;
+  }
+}
